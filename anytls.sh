6f9edb8df173c6b2d47e7c6abd6a80b30137b074
shuijiao1 <74536735+shuijiao1@users.noreply.github.com>
2026-05-11 12:06:38 +0800

    Remove standalone script wrappers

--- anytls.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-exec bash <(curl -fsSL https://raw.githubusercontent.com/shuijiao1/anytls-manager/main/anytls.sh) "$@"
--- realm.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
-exec bash <(curl -fsSL https://raw.githubusercontent.com/shuijiao1/realm-manager/main/realm.sh) "$@"
